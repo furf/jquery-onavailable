4907e36ae71deea4abd30afeb30eb459fc5e87be
David Furfero <furftastic@yahoo.com>
2008-12-23 19:11:15 -0500

    first commit

---
+++ jquery.onavailable.js
@@ -0,0 +1,178 @@
+(function($) {
+
+  $.extend({
+
+
+    /**
+     * Executes the supplied callback when the item with the supplied
+     * id is found.  This is meant to be used to execute behavior as
+     * soon as possible as the page loads.  If you use this after the
+     * initial page load it will poll for a fixed time for the element.
+     * The number of times it will poll and the frequency are
+     * configurable.  By default it will poll for 40 seconds at a rate
+     * of 50 times per second.
+     *
+     * <p>The callback is executed with a single parameter:
+     * the custom object parameter, if provided.</p>
+     *
+     * @method onAvailable
+     * @param {String||Array} el id (or array of ids) of element(s) to look for
+     * @param {Function} callback method to execute when element is found
+     * @param {Object} obj optional object to passed as parameter to callback
+     * @param {Boolean|Object} override If set to true, callback will execute in
+     *                   the scope of obj. If set to an object, callback will
+     *                   execute in the scope of that object
+     * @param checkContent {Boolean} check child node readiness (onContentReady)
+     * @static
+     */
+    onAvailable: function(el, callback /*, obj, override, checkContent */) {
+
+      var self = $.onAvailable;
+
+      if (!(el instanceof Array)) {
+        el = [el];
+      }
+      
+      // Validate callback
+      if (typeof callback !== 'function') {
+        throw new TypeError();
+      }
+            
+      var obj          = arguments[2];
+      var override     = arguments[3];
+      var checkContent = !!arguments[4];
+      
+      // Push listeners onto the stack
+      var listeners = self.listeners;      
+      $.each(el, function(i, id) {
+        listeners.push({
+          id:           id,
+          callback:     callback,
+          obj:          obj,
+          override:     override,
+          checkContent: checkContent
+        });        
+      });
+      
+      // Begin polling for DOM elements
+      if (!self.interval) {
+        self.interval = window.setInterval(self.checkAvailable, self.POLL_INTERVAL);
+      }
+      
+      return this;
+    },
+
+
+    /**
+     * Works the same way as onAvailable, but additionally checks the
+     * state of sibling elements to determine if the content of the
+     * available element is safe to modify.
+     *
+     * <p>The callback is executed with a single parameter:
+     * the custom object parameter, if provided.</p>
+     *
+     * @method onContentReady
+     * @param {String||Array} el id (or array of ids) of element(s) to look for
+     * @param {Function} callback method to execute when element is found
+     * @param {Object} obj optional object to passed as parameter to callback
+     * @param {Boolean|Object} override If set to true, callback will execute in
+     *                   the scope of obj. If set to an object, callback will
+     *                   execute in the scope of that object
+     * @static
+     */
+    onContentReady: function(el, callback, obj, override) {
+      $.onAvailable(el, callback, obj, override, true);
+    }
+    
+  });
+  
+  $.extend($.onAvailable, {
+
+
+    /**
+     * Number of attempts to locate element
+     */
+    POLL_RETRIES: 2000,
+
+
+    /**
+     * Milliseconds between retries
+     */
+    POLL_INTERVAL: 20,
+    
+    
+    /**
+     * setInterval object
+     */
+    interval: null,
+    
+    
+    /**
+     * Array of listeners
+     */
+    listeners: [],
+
+
+    /**
+     * Fires the callback method of the listener in its proper scope
+     *
+     * @method executeListener
+     * @param {DOMElement} el The discovered element
+     * @param {Object} listener The listener object
+     * @return {void}
+     */
+    executeListener: function(el, listener) {
+      // Resolve scope (the value for the this keyword in the callback)
+      var scope = el;
+      if (listener.override) {
+        // Override the scope with obj
+        if (listener.override === true) {
+          scope = listener.obj;
+        } else {
+          // Override the scope with override
+          scope = listener.override;
+        }
+      }
+      // Execute the callback
+      listener.callback.call(scope, listener.obj);
+    },
+
+
+    /**
+     * Polls the DOM for an element matching the supplied id and fires the 
+     * associated callback method
+     *
+     * @method checkAvailable
+     * @return {void}
+     */
+    checkAvailable: function() {
+
+      var self = $.onAvailable;
+      var listeners = self.listeners;
+
+      // Iterate through listener stack
+      $.each(listeners, function(i, listener) {
+        if (listener) {
+
+          // Attempt to find the element in the DOM
+          var el = document.getElementById(listener.id);
+
+          // If element is found and we don't need to check children (onAvailable) 
+          // or DOM is loaded or nextSibling is loaded, then execute the callback
+          // and remove the listener from the stack
+          if (el && (!listener.checkContent || $.isReady || el.nextSibling)) {
+            listeners.splice(i, 1);
+            self.executeListener(el, listener);
+          }
+
+          // Clear interval if all listeners have been executed
+          // or the retry limit has been reached
+          if (listeners.length == 0 || --self.POLL_RETRIES == 0) {
+            self.interval = window.clearInterval(self.interval);
+          }
+        }
+      });
+    }
+  });  
+  
+})(jQuery);
